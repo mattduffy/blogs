e4db59478810d3ef605f34919a17e74afa0dfc6a
Matthew Duffy <mattduffy@gmail.com>
2024-07-20 23:31:30 -0700

    Refactor package entry point so it just exports the main blog classes.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+/**
+ * @module @mattduffy/blogs
+ * @author Matthew Duffy <mattduffy@gmail.com>
+ * @file src/index.js The Blog class definition file.
+ */
+
+import { Blog } from './Blog.js'
+import { Post } from './Post.js'
+import { Blogs } from './Blogs.js'
+import { ObjectId } from '../lib/mongodb-client.js'
+import {
+  _log as Log,
+  _error as _Error,
+} from './utils/debug.js'
+
+const _log = Log.extend('Blog-index')
+const _error = _Error.extend('Blog-index')
+
+export {
+  Blog,
+  Post,
+  Blogs,
+}
