93a9020797272cdea07996441d820016420b5100
Matthew Duffy <mattduffy@gmail.com>
2024-09-08 23:42:06 -0700

    Removed old, commented-out lines and comment out post.save() line in updatePost() method.

--- src/Blog.js
+++ src/Blog.js
@@ -17,22 +17,6 @@ const _log = Log.extend('Blog')
 const _error = _Error.extend('Blog')
 const BLOGS = 'blogs'
 _log(`slug length max: ${MAX_SLUG_LENGTH}`)
-// const MAX_SLUG_LENGTH = process.env.MAX_SLUG_LENGTH || 80
-
-// /**
-//  * Turn a text string into a valid url slug.
-//  * @summary Turn a text string into a valid url slug.
-//  * @author Matthew Duffy <mattduffy@gmail.com>
-//  * @param { String } t - A string of text which may contain non-url compatible characters.
-//  * @return { String } A string with all punctuation chars removed and spaces collapsed into single - char.
-//  */
-// function slugify(t) {
-//   if (!t) return null
-//   return t.toLowerCase() // convert to lowercase letters
-//     .replace(/[^\p{Letter}\p{Number}\p{Separator}]/gv, ' ') // restrict by unicode character classes
-//     .replace(/\s+/g, '-') // collapse multiple spaces to single space and replace with dash
-//     .slice(0, MAX_SLUG_LENGTH) // truncate slug to global variable
-// }
 
 /**
  * A class to model the shape and properties of a blog of posts.
@@ -507,13 +491,13 @@ class Blog {
       blogId: this.#blogId,
       ...p,
     }
-    log(`updating post {_id:${p._id}} with: `, o)
+    log(`updating post {_id:${p.id}} with: `, o)
     let post
     try {
       post = await new Post(this.#mongo, o).init()
       log('updating post instance: ', post.id)
       log(post)
-      post = await post.save()
+      // post = await post.save()
       const update = {
         id: new ObjectId(post.id),
         title: post.title,
@@ -607,6 +591,7 @@ class Blog {
       this.#posts[found].createdOn = u.createdOn
       this.#posts[found].editedOn = u.editedOn
       this.#posts[found].public = u.public
+      this.#posts[found].img = u.img
     }
     try {
       const filter = { _id: this.#blogId }
